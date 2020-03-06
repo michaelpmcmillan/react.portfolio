ad0472ad52ac215d5fe7c3fe2687843ccd336c42
Mike <mike.mcmillan@onthebeach.co.uk>
2020-03-06 14:18:49 +0000

    Added serviceWorker and set to work offline

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './javascript/App';
+import * as serviceWorker from './javascript/serviceWorker';
 import './styles/index.css'
 
 ReactDOM.render(
     <App />, 
     document.getElementById('root'));
+
+serviceWorker.register();
