ad7bbe7acfe8fe1d0df7fd5a192b9d00748b0ecd
Mike <mike.mcmillan@onthebeach.co.uk>
2020-03-06 14:03:06 +0000

    Covert Header to a Component

--- src/javascript/Header.js
+++ src/javascript/Header.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import { faHome, faBook } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../styles/Header.css';
 
-function Header() {
-  return (
-    <div className="header">
-        <Navbar>
-            <Navbar.Brand href="/">Mike McMillan</Navbar.Brand>
-            <Navbar.Collapse>
-                <Nav>
-                    <Nav.Link href="/"><FontAwesomeIcon icon={faHome} /> Home</Nav.Link>
-                    <Nav.Link href="/portfolio"><FontAwesomeIcon icon={faBook} /> Portfolio</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    </div>
-  );
+export default class Header extends Component {
+  render() {
+      return (
+        <div className="header">
+            <Navbar>
+                <Navbar.Brand href="/">Mike McMillan</Navbar.Brand>
+                <Navbar.Collapse>
+                    <Nav>
+                        <Nav.Link href="/"><FontAwesomeIcon icon={faHome} /> Home</Nav.Link>
+                        <Nav.Link href="/portfolio"><FontAwesomeIcon icon={faBook} /> Portfolio</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+      )
+  }
 }
-
-export default Header;
